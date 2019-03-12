5a1ddf36154df2d112db685de2ebf46ef67c4b09
c10t <c10t@fa.ke>
2019-03-13 00:05:54 +0900

    setup gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `Money Log`
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+  ]
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class Index extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    )
+  }
+}
